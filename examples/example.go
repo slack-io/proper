3f746398a502d947c6e4fc30469964ad2f9efdc1
Raed Shomali <shomali11@gmail.com>
2023-11-19 00:06:32 -0500

    Proper

---
+++ examples/example.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/slack-io/proper"
+)
+
+func main() {
+	parameters := make(map[string]string)
+	parameters["boolean"] = "true"
+	parameters["float"] = "1.2"
+	parameters["integer"] = "11"
+	parameters["string"] = "value"
+
+	properties := proper.NewProperties(parameters)
+
+	fmt.Println(properties.BooleanParam("boolean", false))
+	fmt.Println(properties.FloatParam("float", 0))
+	fmt.Println(properties.IntegerParam("integer", 0))
+	fmt.Println(properties.StringParam("string", ""))
+}
